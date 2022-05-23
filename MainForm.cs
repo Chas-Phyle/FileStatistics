9f71afe46109dad62f14b54b40a2e089acc46e51
Charles Phyle <cphyle@mail.valenciacollege.edu>
2022-05-22 21:07:02 -0400

    add new Form with button

---
+++ MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module2
+{
+    class MainForm
+    {
+        static void Main(string[] args)
+        {
+            {
+                Form main = new Form();   //Used to create a nice application view comingsoon!!
+
+                TextBox welcomeScreen = new TextBox()
+                {
+                    Text = "Welcome to my first windows app! V.1"
+                };
+                Button startButton = new Button()
+                {
+                    Text = "Hello",
+                    Location = new System.Drawing.Point(10, 10)
+                };
+                startButton.Click += (o, s) =>
+                    {
+                        Module2.Program.Main(Directory.GetCurrentDirectory());
+                };
+                main.Controls.Add(welcomeScreen);
+                main.Controls.Add(startButton);
+                main.ShowDialog();
+            }
+        }
+    }
+}
